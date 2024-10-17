6463e07088530325784a73f73398cd9d83d0e6be
Pokfinner <finkry@gmail.com>
2024-10-17 20:23:10 +0100

    testing cy.yml

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ function add(a, b) {
   return a + b;
 }
 
+const app = require("express")()
+
+app.get("/", (req, res) => res.send("Server UP."))
+app.listen(3000, () => console.log("Server listening on 3000"))
+
 module.exports = { add };
 
