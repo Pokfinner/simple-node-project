5f3743cd59abad25ba2be682eec71338cded9442
Pokfinner <finkry@gmail.com>
2024-10-17 21:01:30 +0100

    fixing test n

--- test_me.js
+++ test_me.js
@@ -3,10 +3,5 @@ function add(a, b) {
   return a + b;
 }
 
-const app = require("express")()
-
-app.get("/", (req, res) => res.send("Server UP."))
-app.listen(3000, () => console.log("Server listening on 3000"))
-
 module.exports = { add };
 
